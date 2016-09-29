ebc635f34b15419076dfc621181eed008243c9b7
user <user@0101NB244.center.mts.ru>
2016-09-29 17:31:15 +0300

    Доделал вариант как у Жирного Тони по  методу readFile

--- src/mybackup/MyBackUp.java
+++ src/mybackup/MyBackUp.java
@@ -1,7 +1,9 @@
 /*This freeware program.
  */
 package mybackup;
-
+import java.util.Scanner;
+import java.io.File;
+import javax.swing.JOptionPane;
 /**
  *
  * @author kenasv
@@ -11,8 +13,33 @@ public class MyBackUp {
     /**
      * @param args the command line arguments
      */
+    static String[] m = String [5];
+    static Scanner scn;
+
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        openFile();
+        readFile();
+        outFile();
     }
     
+    private static void openFile(){
+        try{
+        scn = new Scanner (new File(D:/1/test.txt));
+        }catch(Exception e) {JOptionPane.showMessageDialog(null, "Файл не найден");}
+    }
+    
+    private static void readFile(){
+        while (scn.hasNext()){
+          for(int row=0;row<m.length;row++){
+              for(int col=0;col<m[row].length;col++){
+              m[row][col] = scn.next();
+              }
+          }  
+        }
+    }
+    private static void outFile(){
+        
+    }
+        
 }
